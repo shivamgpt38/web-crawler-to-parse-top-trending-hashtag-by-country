df178f0b3cd90ee0329609f976aa95371b7edce9
shivam gupta <shivamgpt38@gmail.com>
2017-10-07 04:53:41 -0700

    first commit v0.1

---
+++ app.js
@@ -0,0 +1,54 @@
+const cheerio = require('cheerio');
+const request = require('request');
+const readline = require('readline');
+const chalk = require('chalk');
+
+const rl = readline.createInterface({
+	input:process.stdin,
+	output:process.stdout
+});
+function startingPoing(){
+	rl.question('Enter a Country name:',(countryname) =>{
+		let a = 'https://trends24.in/'+countryname;
+		validate(a);
+	})
+}
+
+
+function validate(url){
+	//let regex = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+	let regex = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9]\.[^\s]{2,})/
+	if(url.match(regex)){
+		startParsing(url);
+	}else{
+		rl.question(chalk.red('Enter a valid URL to crawl:'),(a) =>{
+			validate(a);
+		})
+	}
+}
+
+function startParsing(url){
+	request(url,(error,response,html)=>{
+		if(error) return console.log(chalk.red(error));
+		var $ = cheerio.load(html);
+		for(var i =1 ; i<=10;i++){
+			let str = '#trend-list > div:nth-child(1) > ol > li:nth-child('+i+')>a'
+			console.log(chalk.red($(str).text()));
+		}
+		rest();
+	})
+}
+
+function rest(){
+	rl.question('Restart (y/n):',(e) =>{
+		if(e == 'y' || e == 'Y'){
+			startingPoing();
+		}else if(e == 'n' || e == 'N'){
+			console.log(chalk.red('Bye :-)'))
+			rl.close();
+		}else{
+			rest();
+		}
+	})
+}
+startingPoing();
